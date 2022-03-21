a10bf58a73f6cdb459b2727f4151d11f3d221958
reinardricky <reinardricky@gmail.com>
2022-03-21 22:33:12 +0700

    making my app in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header>
+				<h1>Reinardtify</h1>
+			</header>
+			<div className="music-list">
+				<div className="container">
+					<div className="music">
+						<img src={data.album.images[0].url} alt="album" />
+						<h2>{data.name}</h2>
+						<h3>{data.artists[0].name}</h3>
+						<h4>{data.album.name}</h4>
+						<button className="select">Select</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
