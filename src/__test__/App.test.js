e3d6cedca59d76b9546b7831743c59fb57c6de37
reinardricky <reinardricky@gmail.com>
2022-04-19 18:57:29 +0700

    add informmation in home page

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,18 +1,12 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 import Music from "../components/music";
 import CreatePlaylist from "../pages/create-playlist";
 import { testItems } from "./jsonData/tracks";
-// import { searchItems } from "./jsonData/search";
 import { Provider } from "react-redux";
 import store from "../core/redux/store";
 import SearchBar from "../components/searchbar";
 import userEvent from "@testing-library/user-event";
-// import SignIn from "../components/signIn";
-// import { rest } from "msw";
-// import { setupServer } from "msw/node";
-// import AxiosMockAdapter from "axios-mock-adapter";
-// import axiosMock from "axios";
 
 test("renders headers", () => {
 	render(<App />);
@@ -57,63 +51,3 @@ test("searchBar component", () => {
 
 	expect(searchTrack).toHaveBeenCalled();
 });
-
-// test("user Profile", async () => {
-// 	render(
-// 		<Provider store={store}>
-// 			<SignIn />
-// 		</Provider>
-// 	);
-// });
-
-// test("Seach Api", async () => {
-// 	render(
-// 		<Provider store={store}>
-// 			<CreatePlaylist />
-// 		</Provider>
-// 	);
-
-// 	const input = screen.getByPlaceholderText("Search...");
-// 	userEvent.type(input, "abc{enter}");
-
-// 	// await waitFor(() => screen.getByRole("alert"));
-// 	// expect(screen.getByRole("alert")).toHaveTextContent("Search error");
-
-// 	screen.debug();
-// });
-
-// test("Seach Api", async () => {
-// 	render(<App />);
-// });
-
-// const searchTrack = rest.get(
-// 	"https://api.spotify.com/v1/search?q=abc&type=track",
-// 	(req, res, ctx) => {
-// 		return res(ctx.json(testItems));
-// 	}
-// );
-
-// const handlers = [searchTrack];
-
-// const server = new setupServer(...handlers);
-
-// beforeAll(() => server.listen());
-// afterEach(() => server.resetHandlers());
-// afterAll(() => server.close());
-
-// test("Seach Api", async () => {
-// 	axiosMock.get.mockResolvedValueOnce();
-
-// 	render(
-// 		<Provider store={store}>
-// 			<CreatePlaylist />
-// 		</Provider>
-// 	);
-
-// 	const input = screen.getByPlaceholderText("Search...");
-// 	userEvent.type(input, "abc{enter}");
-// 	screen.debug();
-
-// 	const title = await wait;
-// 	expect(title).toBeInTheDocument();
-// });
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,42 @@
 import SignIn from "../../components/signIn";
+import "./home.css";
 
 function Home() {
 	return (
 		<>
 			<SignIn />
-			<h1>Please login to Search tracks and Create playlist</h1>
+			<h1 className="home-login">
+				Please login to Search tracks and Create playlist
+			</h1>
+			<div className="home-grid">
+				<div className="home-row home-left">
+					<h2>Hello Welcome to Reinardtify !</h2>
+				</div>
+				<div className="home-row home-right">
+					<h2>What is Reinardtify ?</h2>
+				</div>
+				<div className="home-row home-left">
+					<h2>
+						Reinardtify is a Web App to create your desired playlist in spotify
+						!
+					</h2>
+				</div>
+				<div className="home-row home-right">
+					<h2>Wow cool, How do I use it ?</h2>
+				</div>
+				<div className="home-row home-left">
+					<h2>
+						Easy, just click the login to spotify button then you can search and
+						select the tracks you want to insert to your playlist
+					</h2>
+				</div>
+				<div className="home-row home-left">
+					<h2>
+						After you finish picking your songs, just type the title and the
+						description of your playlist and click add playlist
+					</h2>
+				</div>
+			</div>
 		</>
 	);
 }
